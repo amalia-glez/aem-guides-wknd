75b20f80a825a6e16594dcfd7aeefdad66315590
AmaliaGlez <a.gonzalez@codebay-innovation.com>
2020-08-03 16:07:29 +0100

    Path current page

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/Breadcrumb.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/Breadcrumb.java
@@ -1,18 +1,49 @@
 package com.adobe.aem.guides.wknd.core.models;
 
+import com.adobe.xfa.ut.StringUtils;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.apache.sling.settings.SlingSettingsService;
+
+import javax.annotation.PostConstruct;
+import java.util.Optional;
+
+import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_TYPE;
 
 @Model(adaptables = Resource.class)
 public class Breadcrumb {
 
-    public String getText() {
-        return text;
+    @ValueMapValue(name = PROPERTY_RESOURCE_TYPE, injectionStrategy = InjectionStrategy.OPTIONAL)
+    @Default(values = "No resourceType")
+    protected String resourceType;
+
+    @SlingObject
+    private Resource currentResource;
+    @SlingObject
+    private ResourceResolver resourceResolver;
+
+    private String path;
+
+    @PostConstruct
+    protected void init() {
+        PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
+        String currentPagePath = Optional.ofNullable(pageManager)
+                .map(pm -> pm.getContainingPage(currentResource))
+                .map(Page::getPath).orElse("");
+
+        path = currentPagePath;
     }
 
-    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-    protected String text;
+    public String getMessage() {
+        return path;
+    }
 
 }
