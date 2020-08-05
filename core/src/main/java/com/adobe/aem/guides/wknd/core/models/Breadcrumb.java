3b271b87ec520693da3b5cdc03df1e949d1b240a
AmaliaGlez <a.gonzalez@codebay-innovation.com>
2020-08-05 09:06:52 +0100

    Breadcrumb functionality done

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/Breadcrumb.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/Breadcrumb.java
@@ -1,6 +1,5 @@
 package com.adobe.aem.guides.wknd.core.models;
 
-import com.adobe.xfa.ut.StringUtils;
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.PageManager;
 import org.apache.sling.api.resource.Resource;
@@ -8,12 +7,11 @@ import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
-import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
-import org.apache.sling.settings.SlingSettingsService;
-
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_TYPE;
@@ -30,20 +28,48 @@ public class Breadcrumb {
     @SlingObject
     private ResourceResolver resourceResolver;
 
+    private List<Page> pageList = new ArrayList<>();
+
     private String path;
+
+    private String currentPageResult;
+
+    private String pathPage;
+
+    private int length;
 
     @PostConstruct
     protected void init() {
+
         PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
+        Page currentPage = pageManager.getContainingPage(currentResource);
         String currentPagePath = Optional.ofNullable(pageManager)
                 .map(pm -> pm.getContainingPage(currentResource))
                 .map(Page::getPath).orElse("");
-
+        Page currentPageResult = currentPage;
         path = currentPagePath;
+        pathPage = currentPage.getContentResource().getResourceType();
+        length = currentPage.getDepth();
+
+        for (int i = 0; i < length; i++) {
+            pageList.add(currentPage.getAbsoluteParent(i));
+        }
     }
 
-    public String getMessage() {
+    public String getPath() {
         return path;
+    }
+
+    public String getPathPage() {
+        return pathPage;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public List<Page> getPageList() {
+        return pageList;
     }
 
 }
